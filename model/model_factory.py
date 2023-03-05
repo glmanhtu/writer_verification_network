3a02df8a9771369522b916d2b4e069d22c62308c
Manh Tu VU <glmanhtu@gmail.com>
2023-03-05 11:07:01 +0100

    Update network

--- model/model_factory.py
+++ model/model_factory.py
@@ -10,5 +10,5 @@ class ModelsFactory:
 
     @staticmethod
     def get_model(args, working_dir, is_train, device, dropout=0.4):
-        model = simsiam.SimSiam(models.__dict__[args.arch], dim=2048, pred_dim=512)
+        model = simsiam.SimSiam(models.__dict__[args.arch])
         return ModelWrapper(args, working_dir, model, is_train, device)

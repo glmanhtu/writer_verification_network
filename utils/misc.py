cc703c8f1cd909a80dd0ae10d18604d260945b3e
Manh Tu VU <glmanhtu@gmail.com>
2023-04-11 12:35:45 +0200

    Update

--- utils/misc.py
+++ utils/misc.py
@@ -69,8 +69,8 @@ def compute_distance_matrix(data: Dict[str, Tensor], n_times_testing=5):
             source_features = data[source][torch.randint(len(data[source]), (n_times_testing * n_items,))]
             target_features = data[target][torch.randint(len(data[target]), (n_times_testing * n_items,))]
 
-            source_features = F.normalize(source_features, p=2, dim=1)
-            target_features = F.normalize(target_features, p=2, dim=1)
+            # source_features = F.normalize(source_features, p=2, dim=1)
+            # target_features = F.normalize(target_features, p=2, dim=1)
             distance = F.mse_loss(source_features, target_features)
 
             mean_distance = distance.cpu().item()

f043d7f85ba4ae34f09729ab81ea34675c3b20f9
VU Manh Tu <glmanhtu@gmail.com>
2023-06-30 17:11:52 +0200

    Add readme

--- options/base_options.py
+++ options/base_options.py
@@ -32,7 +32,7 @@ class BaseOptions:
                                   help='name of the experiment. It decides where to store samples and models')
         self._parser.add_argument('--name', type=str, default='experiment_1',
                                   help='name of the experiment. It decides where to store samples and models')
-        self._parser.add_argument('--wb_mode', type=str, default='online', help='Wandb sync mode')
+        self._parser.add_argument('--wb_mode', type=str, default='offline', help='Wandb sync mode')
         self._parser.add_argument('--wb_entity', type=str, default='glmanhtu', help='Wandb entity name')
         self._parser.add_argument('--wb_project', type=str, default='writer-verification-network', help='Wandb project')
         self._parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
